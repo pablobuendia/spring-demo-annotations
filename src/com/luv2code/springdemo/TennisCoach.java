b3893d837bcd42efc1d108bc5b6a804fc6ffe26c
Pablo Buendia <buenaspablo@gmail.com>
2018-10-21 23:24:31 -0300

    eight

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// define a default constructor
@@ -13,11 +14,13 @@ public class TennisCoach implements Coach {
 		System.out.println(">> inside default constructor");
 	}
 	
+	/*
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService theFortuneService) {
 		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	/*
 	@Autowired
