4f132bb8ec5d2e356d84715280a25420399772f6
Pablo Buendia <buenaspablo@gmail.com>
2018-10-23 00:51:01 -0300

    fiftieth

--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,16 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
@@ -17,5 +25,13 @@ public class SwimCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}	
 
 }
--- src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -11,7 +11,7 @@ public class SwimJavaConfigDemoApp {
 				new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
@@ -19,7 +19,11 @@ public class SwimJavaConfigDemoApp {
 		// call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
 		
-		// close the context ninth  thirtieth
+		// call our new swim coach methods... has the props values injected
+		System.out.println("email: " + theCoach.getEmail());
+		System.out.println("team: " + theCoach.getTeam());
+		
+		// close the context ninth 
 		context.close();
 	}
 
